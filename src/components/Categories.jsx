8cc0785e10d8f5c3f8bef0902ebd0c8e82265499
Alexandr Naum <mfq07k9cz@gmail.com>
2022-10-15 20:21:56 +0300

    Categories added onClickCategory

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,22 @@
+import React from 'react'
+
 export function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0)
+
+  const categories = ['Всё', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+  const onClickCategory = index => {
+    setActiveIndex(index)
+  }
+
   return (
     <div className='categories'>
       <ul>
-        <li className='active'>Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((category, index) => (
+          <li key={index} onClick={() => onClickCategory(index)} className={activeIndex === index ? 'active' : ''}>
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   )
