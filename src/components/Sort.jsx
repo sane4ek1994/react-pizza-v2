66961b97b2b874c50edae7f491906a04e254aa7a
Alexandr Naum <mfq07k9cz@gmail.com>
2022-10-19 16:50:05 +0300

    Add popup logic in Sort

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,16 @@
+import React from 'react'
+
 export function Sort() {
+  const [open, setOpen] = React.useState(false)
+  const [selected, setSelected] = React.useState(0)
+  const list = ['популярности', 'цене', 'алфавиту']
+  const sortName = list[selected]
+
+  const onClickListItem = index => {
+    setSelected(index)
+    setOpen(false)
+  }
+
   return (
     <div className='sort'>
       <div className='sort__label'>
@@ -9,15 +21,19 @@ export function Sort() {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setOpen(!open)}>{sortName}</span>
       </div>
-      <div className='sort__popup'>
-        <ul>
-          <li className='active'>популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {open && (
+        <div className='sort__popup'>
+          <ul>
+            {list.map((name, index) => (
+              <li onClick={() => onClickListItem(index)} className={selected === index ? 'active' : ''} key={index}>
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
