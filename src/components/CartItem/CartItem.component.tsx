d1846da4ba8517e7437a0daf6aa2c5667bd0ab24
Alexandr Naum <mfq07k9cz@gmail.com>
2022-12-16 18:01:10 +0300

    CartItem fix /slice

--- src/components/CartItem/CartItem.component.tsx
+++ src/components/CartItem/CartItem.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { addItem, minusItem, removeItem } from '../../redux/cart/slice'
-import { CartItem as CartItemType } from '../../redux/slices/cart/types'
+import { CartItem as CartItemType } from '../../redux/cart/types'
 
 type CartItemProps = {
   id: string
