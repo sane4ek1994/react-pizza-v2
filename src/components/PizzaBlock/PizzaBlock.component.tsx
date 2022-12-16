2bfcd562e0b9da67298ab2e0c39973db14bd2c96
Alexandr Naum <mfq07k9cz@gmail.com>
2022-12-16 18:06:04 +0300

    fix PizzaBlock /slice

--- src/components/PizzaBlock/PizzaBlock.component.tsx
+++ src/components/PizzaBlock/PizzaBlock.component.tsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { addItem } from '../../redux/cart/slice'
 import { selectCartItemById } from '../../redux/cart/selectors'
-import { CartItem } from '../../redux/slices/cart/types'
+import { CartItem } from '../../redux/cart/types'
 
 const typeNames = ['тонкое', 'традиционное']
 
